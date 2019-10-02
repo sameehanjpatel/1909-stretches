5f02c88c7bbfd815cb7e71c0c3031edb60f77760
Sameehan Patel <sp4315@nyu.edu>
2019-10-02 18:55:47 -0400

    stretch3

--- questions/04_zip/zip.js
+++ questions/04_zip/zip.js
@@ -6,7 +6,13 @@ zip([
   {x: 2, z: 1},
   {z: 2, y: 1}]) => {x: 3, y: 3, z: 3}
 */
+//came into class a little late
+function zip(objs) {
+  let newObj = {}
+  objs.forEach(
 
-function zip(objs) {}
+  )
+  return newObj
+}
 
 module.exports = { zip };
