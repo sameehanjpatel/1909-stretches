8407a6ad4f83d92108ed3dfad89117c9cedcf655
Sameehan Patel <sp4315@nyu.edu>
2019-11-20 18:43:38 -0500

    adding

--- questions/16_sortOrder/sortOrder.js
+++ questions/16_sortOrder/sortOrder.js
@@ -3,24 +3,34 @@ SortOrder takes an array of objects, a key to be sorted on
 and a key for the return value. 
 */
 
-function sortOrder(arr) {
-    const returnArr = []
+function sortOrder(arr, sorted, key) {
+    // const returnArr = []
     //iterate through the objects and get keys
-    let keys = []
-    const keys = arr.forEach(item =>
-        let currentKeys = Object.keys(item);
-        keys.push(currentKey[0])
-        )
-    //sort objects by keys
-    keys.sort()
-    //loop through key array and add value from object to return array
-    keys.forEach(
-        obj
-    )
-        //I think I should have made a large object with key-name pairs and pushed new keys into return array 
+    // let keys = []
+    // const keys = arr.forEach(item =>
+    //     let currentKeys = Object.keys(item);
+    //     keys.push(currentKey[0])
+    //     )
+    // //sort objects by keys
+    // keys.sort()
+    // //loop through key array and add value from object to return array
+    // keys.forEach(
+    //     obj
+    // )
+    //     //I think I should have made a large object with key-name pairs and pushed new keys into return array 
     
-        return returnArr;
+    //     return returnArr;
 
+    return arr.sort((a,b)=>{
+        if(a[sorted] < b[sorted]){
+            return -1
+        }
+        if (a[sorted] > b[sorted]){
+            return 1
+        } 
+        return 0})
+        .map(element => {
+        return element[key]})
 }
 
 module.exports = { sortOrder };
